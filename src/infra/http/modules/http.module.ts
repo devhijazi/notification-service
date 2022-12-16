eb809beb4d050e658bebcc9a04a42d8bf458ddb5
Gabriel <hijazi.root@gmail.com>
2022-12-16 16:28:29 -0400

    feat: inject dependencies

--- src/infra/http/modules/http.module.ts
+++ src/infra/http/modules/http.module.ts
@@ -1,3 +1,8 @@
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountNotification } from '@application/use-cases/count-notifications';
+import { GetNotification } from '@application/use-cases/get-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
 import { Module } from '@nestjs/common';
 import { SendNotification } from 'src/application/use-cases/send-notification';
 import { DatabaseModule } from 'src/infra/database/database.module';
@@ -7,6 +12,13 @@ import { NotificationsController } from '../controllers/notifications.controller
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountNotification,
+    GetNotification,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
